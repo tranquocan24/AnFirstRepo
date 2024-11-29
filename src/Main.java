27f41f29fd9bc6a5e244380b6aa18c894c95983f
Quoc An <an.tranquoc.cit22@eiu.edu.vn>
2024-11-29 15:05:33 +0700

    Update Input-name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
          public static void main(String[] args) {
-                  System.out.println("Hello An \nWelcome to my repository");
+                  Scanner sc = new Scanner(System.in);
+                  String input = sc.nextLine();
+                  System.out.println("Hello "+ input + " \nWelcome to my repository");
          }
 }
